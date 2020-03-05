4fd44f7c0e0db1ef540766a85757825de120ff50
uchesolomon <solomon7373@yahoo.com>
2020-03-05 16:42:10 +0100

    Views
    Completed views

---
+++ app/src/main/java/com/planetpay/ProvideBankAccountNumberActivity.java
@@ -0,0 +1,14 @@
+package com.planetpay;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ProvideBankAccountNumberActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_provide_bank_account_number);
+    }
+}
---
+++ app/src/main/java/com/planetpay/SecurityQuestionsActivity.java
@@ -0,0 +1,14 @@
+package com.planetpay;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SecurityQuestionsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_security_questions);
+    }
+}
