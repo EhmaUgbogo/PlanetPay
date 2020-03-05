9aa0d561bf65d7a165d9deebf2684e1bb83ddd53
Osemwingie Oshodin <osemwingieoshodin@gmail.com>
2020-03-05 13:35:58 +0100

    Login and register views

---
+++ app/src/main/java/com/planetpay/views/ui/funds_transfer/AccountNumberFragment.java
@@ -0,0 +1,35 @@
+package com.planetpay.views.ui.funds_transfer;
+
+import android.content.Context;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.planetpay.R;
+
+public class AccountNumberFragment extends Fragment {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_account_number, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+    }
+}
