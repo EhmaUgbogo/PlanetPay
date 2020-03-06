59398dcfb8cd9c0673cce64e45128fa920465bda
Osemwingie Oshodin <osemwingieoshodin@gmail.com>
2020-03-06 04:00:02 +0100

    Implemented Registration

--- app/src/main/java/com/planetpay/views/ui/login_register/LoginActivity.java
+++ app/src/main/java/com/planetpay/views/ui/login_register/LoginActivity.java
@@ -1,17 +1,29 @@
 package com.planetpay.views.ui.login_register;
 
-import androidx.appcompat.app.AppCompatActivity;
+import androidx.annotation.NonNull;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.planetpay.BaseActivity;
 import com.planetpay.R;
 
-public class LoginActivity extends AppCompatActivity {
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
-    private EditText email_edit, password_edit;
-    private Button login_btn, sign_up_btn;
+public class LoginActivity extends BaseActivity {
+
+    private static final String TAG = "planetpay.LOGINACTIVITY";
+    private EditText emailEditText, passwordEditText;
+    private Button loginButton, signUpButton;
+    private ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,11 +34,82 @@ public class LoginActivity extends AppCompatActivity {
 
     private void initViews() {
 
-        email_edit = findViewById(R.id.activity_sign_in_email_editText);
-        password_edit = findViewById(R.id.activity_sign_in_password_editText);
+        emailEditText = findViewById(R.id.activity_sign_in_email_editText);
+        passwordEditText = findViewById(R.id.activity_sign_in_password_editText);
 
-        login_btn = findViewById(R.id.activity_sign_in_button_sign_in);
-        sign_up_btn = findViewById(R.id.activity_sign_in_button_sign_up);
+        loginButton = findViewById(R.id.activity_sign_in_button_sign_in);
+        signUpButton = findViewById(R.id.activity_sign_in_button_sign_up);
+        progressBar = findViewById(R.id.activity_sign_in_progressBar);
 
     }
+    private void loginUser(String email, String password){
+        showProgress();
+        if (validateInputs(email, password)){
+            auth.signInWithEmailAndPassword(email, password).addOnCompleteListener(this, new OnCompleteListener<AuthResult>(){
+
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    Log.d(TAG, "User created successfully" + task.isSuccessful());
+                    if (task.isSuccessful()){
+
+
+                    }else {
+                        hideProgress();
+                    }
+
+                }
+            });
+        }else {
+            showToast("Check required inputs");
+            hideProgress();
+        }
+    }
+
+    private boolean validateInputs(String email, String password) {
+        boolean status=true;
+
+        if(!isValidEmail(email)){
+            emailEditText.setError("A valid Email Required");
+            status =false;
+        }
+
+        if(!isValidPassword(password)){
+            passwordEditText.setError("Password of 8 characters & above Required");
+            status= false;
+        }
+
+        return status;
+    }
+
+
+
+    // validating email id
+    public boolean isValidEmail(String email) {
+        String EMAIL_PATTERN = "^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@"
+                + "[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$";
+
+        Pattern pattern = Pattern.compile(EMAIL_PATTERN);
+        Matcher matcher = pattern.matcher(email);
+        return matcher.matches();
+    }
+
+
+    // validating password with retype password
+    public boolean isValidPassword(String pass) {
+        return pass != null && pass.length() >= 8;
+    }
+
+    public void showProgress(){
+        progressBar.setVisibility(View.VISIBLE);
+        loginButton.setEnabled(false);
+        signUpButton.setEnabled(false);
+    }
+
+    public void hideProgress(){
+        progressBar.setVisibility(View.INVISIBLE);
+        loginButton.setEnabled(true);
+        signUpButton.setEnabled(true);
+
+    }
+
 }
---
+++ app/src/main/java/com/planetpay/views/ui/login_register/ValidateFilter.java
@@ -0,0 +1,28 @@
+package com.planetpay.views.ui.login_register;
+
+import android.text.InputFilter;
+import android.text.Spanned;
+import android.util.Log;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+
+public class ValidateFilter implements InputFilter {
+
+    @Override
+    public CharSequence filter(CharSequence charSequence, int start, int end, Spanned spanned, int dstart , int dend) {
+        for ( int i = start ; i < end ; i++) {
+            String checkMe = String. valueOf (charSequence.charAt(i)) ;
+            Pattern pattern =
+                    Pattern. compile ( "[ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz123456789_]*" ) ;
+            Matcher matcher = pattern.matcher(checkMe) ;
+            boolean valid = matcher.matches() ;
+            if (!valid) {
+                Log. d ( "" , "invalid" ) ;
+                return "" ;
+            }
+        }
+        return null;
+    }
+}
